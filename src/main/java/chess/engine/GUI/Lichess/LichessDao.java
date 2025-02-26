019e4d639af4eea0fcd91da0f9a927c4c6127679
Viktor <hrhaarmark@gmail.com>
2025-02-26 22:51:04 +0100

    Lichess factorization

--- src/main/java/chess/engine/GUI/Lichess/LichessDao.java
+++ src/main/java/chess/engine/GUI/Lichess/LichessDao.java
@@ -4,6 +4,7 @@ import chess.engine.GUI.Lichess.Events.GameUpdate;
 import chess.engine.GUI.Lichess.Events.LichessEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -24,7 +25,6 @@ public class LichessDao {
 
 
     static void streamEvents() {
-        System.out.println(API_TOKEN);
         String url = LICHESS_BASE_URL + STREAM_EVENT_PATH;
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = buildGetEvent(url);
@@ -47,6 +47,7 @@ public class LichessDao {
 
     public static void streamGame(String gameId) {
         String url = LICHESS_BASE_URL + STREAM_GAME_PATH + gameId;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = buildGetEvent(url);
 
@@ -72,19 +73,13 @@ public class LichessDao {
 
     static void sendMove(String gameId, String move) {
         String url = LICHESS_BASE_URL + SEND_MOVE_PATH.replace("{gameId}", gameId).replace("{move}", move);
-        HttpClient client = HttpClient.newHttpClient();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .header("Authorization", "Bearer " + API_TOKEN)
-                .POST(HttpRequest.BodyPublishers.noBody())
-                .build();
 
         try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = sendGetEvent(url);
             if (response.statusCode() == 200) {
                 System.out.println("Move played: " + move);
             } else {
+                System.err.println(response.statusCode());
                 System.err.println("Failed to play move. Status: " + response.statusCode());
                 System.err.println("Response: " + response.body());
             }
@@ -96,14 +91,13 @@ public class LichessDao {
 
     static void acceptChallenge(String challengeId) {
         String url = LICHESS_BASE_URL + CHALLENGE_PATH.replace("{challengeId}", challengeId);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = buildPOSTEvent(url, HttpRequest.BodyPublishers.noBody());
 
         try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = sendGetEvent(url);
             if (response.statusCode() == 200) {
                 System.out.println("Accepted challenge: " + challengeId);
             } else {
+                System.err.println(response.statusCode());
                 System.err.println("Failed to accept challenge: " + challengeId);
                 System.err.println("Response: " + response.body());
             }
@@ -112,12 +106,24 @@ public class LichessDao {
         }
     }
 
+    private static HttpResponse<String> sendGetEvent(String url) throws InterruptedException, IOException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = buildPOSTEvent(url, HttpRequest.BodyPublishers.noBody());
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
     private static HttpRequest buildGetEvent(String url) {
         return HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .header("Authorization", "Bearer " + LichessDao.API_TOKEN)
                 .GET()
                 .build();
+    }
+
+    private static HttpResponse<String> sendPostEvent(String url) throws InterruptedException, IOException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = buildPOSTEvent(url, HttpRequest.BodyPublishers.noBody());
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
     private static HttpRequest buildPOSTEvent(String url, HttpRequest.BodyPublisher body) {
@@ -132,12 +138,8 @@ public class LichessDao {
     private void upgradeAccount() {//If I need to upgrade an account again
         final String url = LICHESS_BASE_URL + BOT_UPGRADE_PATH;
 
-        HttpClient client = HttpClient.newHttpClient();
-
-        HttpRequest request = buildPOSTEvent(url, HttpRequest.BodyPublishers.noBody());
-
         try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = sendPostEvent(url);
             System.out.println("HTTP Status Code: " + response.statusCode());
             System.out.println("Response Body: " + response.body());
 
@@ -152,12 +154,10 @@ public class LichessDao {
     }
 
     static void acceptDrawOffer(String gameId, boolean accept) {
-        String toTakeOrNotToTake = (accept) ? "true" : "false";
-        final String url = LICHESS_BASE_URL + DRAW_PATH.replace("{gameId}", gameId).replace("{accept}", toTakeOrNotToTake);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = buildPOSTEvent(url, HttpRequest.BodyPublishers.noBody());
+        String toDrawOrNotToDraw = (accept) ? "true" : "false";
+        final String url = LICHESS_BASE_URL + DRAW_PATH.replace("{gameId}", gameId).replace("{accept}", toDrawOrNotToDraw);
         try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = sendPostEvent(url);
             System.out.println("HTTP Status Code: " + response.statusCode());
             System.out.println("Response Body: " + response.body());
 
@@ -168,6 +168,7 @@ public class LichessDao {
                     System.out.println("Draw offer was declined");
                 }
             } else {
+                System.err.println(response.statusCode());
                 System.err.println("Failed to handle draw offer.");
             }
         } catch (Exception e) {
